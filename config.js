434859c0dcca8cc534cc715738624c672c48518b
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-11 09:45:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['60147366955', '60147366955'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -30,10 +30,10 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = 'Putbotz'
+global.author = 'ganteng'
 
-global.wm = '© ariffb & melcanz'
+global.wm = '© Putbotz'
 global.wait = '_*tunggu sedang diproses...*_'
 global.eror = '_*Server Error*_'
 global.benar = '✅'
