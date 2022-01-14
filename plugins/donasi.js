c29e45b84f719e655a09ff43778fecc7fdb38992
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-14 23:10:21 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,8 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
-┌「 *Donasi • Emoney* 」
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+┌「 *Topap • Maxis* 」
+├ 60147366955
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '© Putbotz', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
