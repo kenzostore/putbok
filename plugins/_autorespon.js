9d82a2d23e54f3b3b433eedaa0dcb5a254cd4b6e
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-15 10:30:37 +0800

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -9,8 +9,14 @@ handler.all = async function (m, { conn, isBlocked }) {
     let { banned } = db.data.users[m.sender]
 
 
-
+    //test
+    let reg = /(as?lm)
+    let isSalam = ref.exec(m.text)
+    if (isSalam && !m.FromMe) {
+        m.reply(`وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n_wa\'alaikumussalam wr.wb._`)
     // salam
+    }
+    
     let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم)/i
     let isSalam = reg.exec(m.text)
     if (isSalam && !m.fromMe) {
