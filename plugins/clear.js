36da660e5de3519d07cba540518340cfef01f808
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-17 23:43:39 +0800

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -1,37 +1,25 @@
 let handler = async (m, { conn, command, args }) => {
-  let chats
-  if (/gro?up|gc/i.test(args[0])) chats = conn.chats.array.filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
-  else if (/chat|private|pc/i.test(args[0])) chats = conn.chats.array.filter(v => v.jid.endsWith('.net') && !v.pin).map(v => v.jid)
-  else if (/all/i.test(args[0])) chats = conn.chats.array.filter(v => v.jid && !v.pin).map(v => v.jid)
-  else chats = [m.chat]
-  let isDelete = /^(delete)/i.test(command)
-  let isClear = /^(clear)/i.test(command)
-  m.reply(`me${isDelete ? 'nghapus' : isClear ? 'mbersihkan' : 'mbisukan'} ${chats.length} chat ${args[0] ? args[0] : ''}`)
+  let chats = args.length > 0 && /group|gc/i.test(args[0]) ? conn.chats.array.filter(v => v.jid.endsWith('g.us') && !v.pin).map(v => v.jid) : [m.chat]
+  let isDelete = /^(clear|delete)/i.test(command)
   for (let id of chats) {
-    if (isDelete || isClear) await conn.modifyChat(id, (isDelete ? 'delete' : 'clear'), {
-      includeStarred: false
-    }).catch(console.log)
-    else await conn.modifyChat(id, 'mute', -Math.floor(new Date / 1e3) * 1e3 - 1e3).catch(console.log)
+    if (isDelete) await conn.modifyChat(id, 'delete').catch(console.log)
+    await conn.modifyChat(id, 'mute', -Math.floor(new Date / 1e3) * 1e3 - 1e3).catch(console.log)
   }
-  m.reply(`*Selesai*`)
+  conn.reply(m.chat, chats.length + ' chat grup telah dib' + (isDelete ? 'ersihkan' : 'isukan selamanya'), m)
 }
-handler.help = [
-  'clearchat',
-  'clearchat chat',
-  'clearchat group',
-  'clearchat all',
-  'deletechat',
-  'deletechat chat',
-  'deletechat group',
-  'deletechat all',
-  'mutechat',
-  'mutechat chat',
-  'mutechat group',
-  'mutechat all'
-]
+handler.help = ['deletechat', 'deletechat group', 'mutechat', 'mutechat group']
 handler.tags = ['owner']
 handler.command = /^(clear|delete|mute)chat$/i
-
 handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
 
 module.exports = handler
+
