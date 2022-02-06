de6f04abddab2146baa20f7058e0703fd0dda31b
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-02-06 14:43:14 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -187,7 +187,18 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       return conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
         "listMessage": {
           "title": `${ucapan()}, ${name}`.trim(),
-          "description": "© Putbotz",
+          "description": "─━─「Putbotz」━⬣
+│⬡ ⏰Aktif selama ${uptime}
+┃⬡ ⚡Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
+│⬡ ⚜️Putbotz
+┃
+│⬡ *${conn.blocklist.length}* Terblock
+┃⬡ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
+│⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
+┃
+│⬡ note : gaada apikey
+┃
+┗─━─━─━─━━⬣`.trim(),",
           "footerText": "Whatsapp Bot",
           "buttonText": "Klik Disini",
           "listType": "SINGLE_SELECT",
